b9f4c6b5ac4435e59ec04e39fbbf493196541af4
shohan89 <maffin.killer007@gmail.com>
2025-08-28 01:12:24 +0600

    Setup three col Home layout

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,12 +1,24 @@
 import Header from "../Shared/Header/Header";
+import LeftSideNav from "../Shared/LeftSideNav/LeftSideNav";
 import Navbar from "../Shared/Navbar/Navbar";
+import RIghtSideNav from "../Shared/RightSideNav/RIghtSideNav";
 
 const Home = () => {
     return (
         <div>
             <Header />
             <Navbar />
-            <h2 className="font-poppins">This is home page</h2>
+            <div className="grid grid-cols-1 md:grid-cols-4 gap-6">
+                <div>
+                    <LeftSideNav />
+                </div>
+                <div className="md:col-span-2">
+                    <h2>News coming soon...</h2>
+                </div>
+                <div>
+                    <RIghtSideNav />
+                </div>
+            </div>
         </div>
     );
 };
