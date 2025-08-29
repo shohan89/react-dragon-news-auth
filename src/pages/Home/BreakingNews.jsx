3de419ec89e7b3ea0bba516a2a204e98410ec50b
shohan89 <maffin.killer007@gmail.com>
2025-08-29 12:28:11 +0600

    Install React Marquee to show breaking news

--- src/pages/Home/BreakingNews.jsx
+++ src/pages/Home/BreakingNews.jsx
@@ -1,8 +1,16 @@
+import Marquee from "react-fast-marquee";
+import { Link } from "react-router";
 
 const BreakingNews = () => {
     return (
-        <div>
+        <div className="flex">
             <button className='btn btn-secondary'>Breaking News</button>
+            <Marquee pauseOnHover={true} speed={100} >
+                <Link className="mr-12" to='/'>I can be a React component, multiple React components, or just some text...</Link>
+                <Link className="mr-12" to='/'>I can be a React component, multiple React components, or just some text...</Link>
+                <Link className="mr-12" to='/'>I can be a React component, multiple React components, or just some text...</Link>
+                <Link className="mr-12" to='/'>I can be a React component, multiple React components, or just some text...</Link>
+            </Marquee>
         </div>
     );
 };
