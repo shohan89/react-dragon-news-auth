0e9259418f46666d688141eeff0e04728730cb3c
Sharoar Hossain <maffin.killer007@gmail.com>
2025-08-30 13:38:42 +0500

    add icons on Login Buttons and add social media handles.

--- src/pages/Shared/RightSideNav/RIghtSideNav.jsx
+++ src/pages/Shared/RightSideNav/RIghtSideNav.jsx
@@ -1,3 +1,4 @@
+import { FaFacebook, FaGithub, FaGoogle, FaInstagram, FaTwitter } from "react-icons/fa";
 
 const RIghtSideNav = () => {
     return (
@@ -5,8 +6,30 @@ const RIghtSideNav = () => {
             {/* Login Buttons */}
             <div className="space-y-4 mb-4">
                 <h2 className="text-2xl font-bold">Login With</h2>
-                <button className="btn btn-outline btn-primary w-full">Login With Google</button>
-                <button className="btn btn-outline w-full">Login With GitHub</button>
+                <button className="btn btn-outline btn-primary w-full">
+                    <FaGoogle />
+                    Login With Google
+                    </button>
+                <button className="btn btn-outline w-full">
+                    <FaGithub />
+                    Login With GitHub
+                    </button>
+            </div>
+            {/* Social Handles */}
+            <div className=" mb-4 p-4">
+                <h2 className="text-2xl font-bold mb-4">Find Us On</h2>
+                <a className="p-4 flex text-lg items-center border rounded-t-lg" href="#">
+                    <FaFacebook className="mr-3" />
+                    Facebook
+                </a>
+                <a className="p-4 flex text-lg items-center border-x " href="#">
+                    <FaTwitter className="mr-3" />
+                    Twitter
+                </a>
+                <a className="p-4 flex text-lg items-center border rounded-b-lg" href="#">
+                    <FaInstagram className="mr-3" />
+                    Instagram
+                </a>
             </div>
         </div>
     );
