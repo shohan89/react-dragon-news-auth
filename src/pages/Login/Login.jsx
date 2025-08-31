5ff31ff5b66900850e64444793d05468345e11db
shohan89 <maffin.killer007@gmail.com>
2025-08-31 23:58:58 +0600

    add login form

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -4,7 +4,19 @@ const Login = () => {
     return (
         <div>
             <Navbar />
-            <h2>This is login.</h2>
+            <div>
+                <h2>Please Login.</h2>
+                <form className='lg:w-1/2 md:w-3/4 mx-auto'>
+                    <fieldset className="fieldset">
+                        <label className="label">Email</label>
+                        <input type="email" className="input" placeholder="Email" />
+                        <label className="label">Password</label>
+                        <input type="password" className="input" placeholder="Password" />
+                        <div><a className="link link-hover">Forgot password?</a></div>
+                        <button className="btn btn-neutral mt-4">Login</button>
+                    </fieldset>
+                </form>
+            </div>
         </div>
     );
 };
