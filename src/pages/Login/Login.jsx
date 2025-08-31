6b4b71809be6ed598f5abd974588bdc5edb00657
shohan89 <maffin.killer007@gmail.com>
2025-09-01 00:07:43 +0600

    Fix Login input width

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -5,13 +5,13 @@ const Login = () => {
         <div>
             <Navbar />
             <div>
-                <h2>Please Login.</h2>
-                <form className='lg:w-1/2 md:w-3/4 mx-auto'>
+                <h2 className='text-2xl font-bold text-center'>Please Login.</h2>
+                <form className='lg:w-1/2 md:w-3/4 mx-auto my-10'>
                     <fieldset className="fieldset">
                         <label className="label">Email</label>
-                        <input type="email" className="input" placeholder="Email" />
+                        <input type="email" className="input w-full" placeholder="Email" />
                         <label className="label">Password</label>
-                        <input type="password" className="input" placeholder="Password" />
+                        <input type="password" className="input w-full" placeholder="Password" />
                         <div><a className="link link-hover">Forgot password?</a></div>
                         <button className="btn btn-neutral mt-4">Login</button>
                     </fieldset>
