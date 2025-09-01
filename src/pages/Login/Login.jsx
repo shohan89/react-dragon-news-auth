f45df2ec45de300764a4f8f95d8aa10c3e601f98
Sharoar Hossain <maffin.killer007@gmail.com>
2025-09-01 12:51:37 +0500

    Add name to each input field and an event handler to login

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,17 +1,23 @@
 import Navbar from '../Shared/Navbar/Navbar';
 
 const Login = () => {
+    const handleLogin = e => {
+        e.preventDefault();
+        const form = new FormData(e.target);
+        const email = form.get('email');
+        console.log("🚀 ~ handleLogin ~ email:", email)
+    }
     return (
         <div>
             <Navbar />
             <div>
                 <h2 className='text-2xl font-bold text-center'>Please Login.</h2>
-                <form className='lg:w-1/2 md:w-3/4 mx-auto my-10'>
+                <form onSubmit={handleLogin} className='lg:w-1/2 md:w-3/4 mx-auto my-10'>
                     <fieldset className="fieldset">
                         <label className="label">Email</label>
-                        <input type="email" className="input w-full" placeholder="Email" />
+                        <input type="email" name='email' className="input w-full" placeholder="Email" />
                         <label className="label">Password</label>
-                        <input type="password" className="input w-full" placeholder="Password" />
+                        <input type="password" name='password' className="input w-full" placeholder="Password" />
                         <div><a className="link link-hover">Forgot password?</a></div>
                         <button className="btn btn-primary mt-4">Login</button>
                     </fieldset>
