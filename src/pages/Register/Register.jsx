b419fa94ab120cbbd93155ec0a6f4650d18e62b0
Sharoar Hossain <maffin.killer007@gmail.com>
2025-09-01 13:07:58 +0500

    Add name & photo url field and change some texts.

--- src/pages/Register/Register.jsx
+++ src/pages/Register/Register.jsx
@@ -6,17 +6,21 @@ const Register = () => {
         <div>
             <Navbar />
             <div>
-                <h2 className='text-2xl font-bold text-center'>Please Register.</h2>
+                <h2 className='text-2xl font-bold text-center'>Please Register</h2>
                 <form className='lg:w-1/2 md:w-3/4 mx-auto my-10'>
-                    <fieldset className="fieldset">
+                    <fieldset className="fieldset space-y-2">
+                        <label className="label">Name</label>
+                        <input type="text" name='name' className="input w-full" placeholder="Your name" />
+                        <label className="label">Photo URL</label>
+                        <input type="text" name='photo' className="input w-full" placeholder="Your photo url" />
                         <label className="label">Email</label>
                         <input type="email" name='email' className="input w-full" placeholder="Email" />
                         <label className="label">Password</label>
                         <input type="password" name='password' className="input w-full" placeholder="Password" />
-                        <button className="btn btn-primary mt-4">Login</button>
+                        <button className="btn btn-primary mt-4">Register</button>
                     </fieldset>
                 </form>
-                <p className='text-center'>Do not have account? Please <Link className='font-bold text-primary' to='/register'>Register</Link></p>
+                <p className='text-center'>Already have an account? Please <Link className='font-bold text-primary' to='/login'>Login</Link></p>
             </div>
         </div>
     );
