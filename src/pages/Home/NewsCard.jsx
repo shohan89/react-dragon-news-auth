589bcafef5eaa7494c9d3d2390cd9c36d453f506
Sharoar Hossain <maffin.killer007@gmail.com>
2025-09-02 19:04:31 +0500

    Show news title, img and description to the UI

--- src/pages/Home/NewsCard.jsx
+++ src/pages/Home/NewsCard.jsx
@@ -1,9 +1,20 @@
 
 const NewsCard = ({ aNews }) => {
-    const { title } = aNews;
+    const { title, thumbnail_url, details } = aNews;
     return (
-        <div>
-            <h2>{title}</h2>
+        <div className="card bg-base-100  shadow-sm">
+            <figure>
+                <img
+                src={thumbnail_url}
+                alt="Shoes" />
+            </figure>
+            <div className="card-body">
+                <h2 className="card-title">{title}</h2>
+                <p>{details.slice(0, 500)+ '...'}</p>
+                <div className="card-actions justify-end">
+                    <button className="btn btn-primary">Buy Now</button>
+                </div>
+            </div>
         </div>
     );
 };
