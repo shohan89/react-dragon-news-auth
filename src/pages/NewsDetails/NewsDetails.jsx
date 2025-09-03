fbe5a6078856671355e34d5110a753a0274a5971
shohan89 <maffin.killer007@gmail.com>
2025-09-03 23:36:58 +0600

    add header on news details page

--- src/pages/NewsDetails/NewsDetails.jsx
+++ src/pages/NewsDetails/NewsDetails.jsx
@@ -1,7 +1,9 @@
+import Header from "../Shared/Header/Header";
 
 const NewsDetails = () => {
     return (
         <div>
+            <Header />
             <h2>This is news details.</h2>
         </div>
     );
