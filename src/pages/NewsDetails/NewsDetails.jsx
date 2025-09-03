cbccdf65c09e1681a704b0c21317aab560a47328
shohan89 <maffin.killer007@gmail.com>
2025-09-03 23:39:25 +0600

    create the layout and add Right sidebar

--- src/pages/NewsDetails/NewsDetails.jsx
+++ src/pages/NewsDetails/NewsDetails.jsx
@@ -1,10 +1,18 @@
 import Header from "../Shared/Header/Header";
+import RIghtSideNav from "../Shared/RightSideNav/RIghtSideNav";
 
 const NewsDetails = () => {
     return (
         <div>
             <Header />
-            <h2>This is news details.</h2>
+            <div className="grid md:grid-cols-4">
+                <div className="col-span-3">
+                    <h2>News details will here...</h2>
+                </div>
+                <div>
+                    <RIghtSideNav />
+                </div>
+            </div>
         </div>
     );
 };
