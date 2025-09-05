ca111c953458dcb28cb55c3a98ad379c25d12489
shohan89 <maffin.killer007@gmail.com>
2025-09-05 15:31:24 +0600

    Add navbar to NewsDetails page

--- src/pages/NewsDetails/NewsDetails.jsx
+++ src/pages/NewsDetails/NewsDetails.jsx
@@ -1,10 +1,12 @@
 import Header from "../Shared/Header/Header";
+import Navbar from "../Shared/Navbar/Navbar";
 import RIghtSideNav from "../Shared/RightSideNav/RIghtSideNav";
 
 const NewsDetails = () => {
     return (
         <div>
             <Header />
+            <Navbar />
             <div className="grid md:grid-cols-4">
                 <div className="col-span-3">
                     <h2>News details will here...</h2>
