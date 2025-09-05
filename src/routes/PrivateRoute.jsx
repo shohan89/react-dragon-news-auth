7d50dff3c1d8c7294aaf951f8602f833c96b998d
shohan89 <maffin.killer007@gmail.com>
2025-09-05 16:05:28 +0600

    add location

--- src/routes/PrivateRoute.jsx
+++ src/routes/PrivateRoute.jsx
@@ -1,9 +1,11 @@
 import { useContext } from 'react';
-import { Navigate } from 'react-router';
+import { Navigate, useLocation } from 'react-router';
 import { AuthContext } from '../providers/AuthProvider';
 
 const PrivateRoute = ({ children }) => {
     const {user, loading} = useContext(AuthContext);
+    const location = useLocation();
+    console.log("🚀 ~ PrivateRoute ~ location:", location)
     // check loading state
     if(loading){
         return <span className="loading loading-spinner text-warning"></span>
