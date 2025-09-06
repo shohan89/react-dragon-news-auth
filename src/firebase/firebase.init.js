7f8c8a0ceb4de27ae0c44d35a1ea999b6b72d81d
Sharoar Hossain <maffin.killer007@gmail.com>
2025-09-06 19:28:56 +0500

    updated firebase config

--- src/firebase/firebase.init.js
+++ src/firebase/firebase.init.js
@@ -6,12 +6,12 @@ import { getAuth } from "firebase/auth";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyBOrEB1Xp6WVRwb3Q0F1MqcI8PTZK2Y2DQ",
-  authDomain: "react-dragon-news-auth-d3850.firebaseapp.com",
-  projectId: "react-dragon-news-auth-d3850",
-  storageBucket: "react-dragon-news-auth-d3850.firebasestorage.app",
-  messagingSenderId: "145227690947",
-  appId: "1:145227690947:web:aeb9092d32133ca1988376"
+  apiKey: import.meta.env.VITE_APIKEY,
+  authDomain: import.meta.env.VITE_AUTHDOMAIN,
+  projectId: import.meta.env.VITE_PROJECTID,
+  storageBucket: import.meta.env.VITE_STORAGEBUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGINGSENDERID,
+  appId: import.meta.env.VITE_APPID
 };
 
 // Initialize Firebase
